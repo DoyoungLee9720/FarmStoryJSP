c9dd5fb9b9b23f6157be545791aa5342398008e5
under912 <under912@naver.com>
2024-08-26 17:12:04 +0900

    KJG_2024_08_26_AdminUserList

--- FarmStoryJSP/src/main/java/com/farmstory/controller/admin/UserListController.java
+++ FarmStoryJSP/src/main/java/com/farmstory/controller/admin/UserListController.java
@@ -1,6 +1,13 @@
 package com.farmstory.controller.admin;
 
 import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.farmstory.dto.UserDto;
+import com.farmstory.service.UserService;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -12,9 +19,16 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/admin/user/list.do")
 public class UserListController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-
+	private UserService service = UserService.INSTANCE;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		List<UserDto> users = service.selectUsers();
+		req.setAttribute("users", users);
+		logger.debug("user 수 : " + users.size());
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/admin/user/list.jsp");
 		dispatcher.forward(req, resp);
 	}
