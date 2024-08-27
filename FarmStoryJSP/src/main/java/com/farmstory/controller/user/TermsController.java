101fab030f9398ea64b88627116e0b1c91dbb7f0
wlgus5946 <wlgus5946@naver.com>
2024-08-27 17:51:47 +0900

    JJH7_2024_08_27_register_update

--- FarmStoryJSP/src/main/java/com/farmstory/controller/user/TermsController.java
+++ FarmStoryJSP/src/main/java/com/farmstory/controller/user/TermsController.java
@@ -9,15 +9,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns =  {"/user/login.do"})
-public class LoginController extends HttpServlet{
+@WebServlet(urlPatterns = {"/user/terms.do"})
+public class TermsController extends HttpServlet{
 
 
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/login.jsp");
+		
+		//테이블 생성후 데이터를 넣어서 전송할 예정
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/user/terms.jsp");
 		dispatcher.forward(req, resp);
 	}
 
--- FarmStoryJSP/src/main/java/com/farmstory/service/UserService.java
+++ FarmStoryJSP/src/main/java/com/farmstory/service/UserService.java
@@ -1,6 +1,16 @@
 package com.farmstory.service;
 
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.farmstory.dao.UserDao;
 import com.farmstory.dto.UserDto;
@@ -26,9 +36,14 @@ public enum UserService {
 		return currentPage;
 	}
 	
-	public int selectUserCount() {
+	public int selectUserCount() { 
 		return dao.selectUserCount();
 	}
+	
+	public int selectCountCheckUser(String type, String value) {
+		return dao.selectCountCheckUser(type, value);
+	}
+	
 	
 	public void insertUser(UserDto user) {
 		dao.insertUser(user);
@@ -45,4 +60,61 @@ public enum UserService {
 	public void deleteUser(String userId) {
 		dao.deleteUser(userId);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String sendEmailCode(String email) {
+		
+		
+		//인증코드 생성
+		int code =ThreadLocalRandom.current().nextInt(100000, 1000000);
+		
+		
+		//이메일 기본정보
+		String title = "FarmStory 이메일 인증코드입니다.";
+		String content = "<h1>인증코드는 " +code+ "입니다.</h1>";
+		String sender = "wlgus5946@gmail.com";
+		String appPass = "dqys begj jhxb ffhs"; //Google 앱 비밀번호
+		
+		// gmail SMTP 설정
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.ssl.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		//gmail session 생성
+		Session gmailSession = Session.getInstance(props, new Authenticator(){
+			
+			@Override // sender, appPass 반환
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication(){ //서버에 로그인시 사용자의 이메일 주소와 비밀번호를 제공하는 역할
+				return new PasswordAuthentication(sender, appPass);
+			}
+		});
+		
+		//메일발송
+		Message message = new MimeMessage(gmailSession);
+		try{
+			message.setFrom(new InternetAddress(sender, "FarmStory", "UTF-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+			message.setSubject(title);
+			message.setContent(content, "text/html;charset=utf-8");
+			
+			Transport.send(message);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return ""+code; //string으로 받기 위해서
+	
+	}
+	
+	
+	
 }
