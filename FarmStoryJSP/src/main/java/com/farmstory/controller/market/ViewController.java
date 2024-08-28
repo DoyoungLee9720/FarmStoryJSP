eba4800dfb8715b0482e10052cf5155875609c95
DoyoungLee9720 <dlehdud224@gmail.com>
2024-08-28 14:00:07 +0900

    LDY_2024_08_28_NewCart_ViewUpdated

--- FarmStoryJSP/src/main/java/com/farmstory/controller/market/ViewController.java
+++ FarmStoryJSP/src/main/java/com/farmstory/controller/market/ViewController.java
@@ -30,24 +30,24 @@ public class ViewController extends HttpServlet{
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/market/view.jsp");
 		dispatcher.forward(req, resp);
 	}
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	    ProductDto productDto = (ProductDto) req.getSession().getAttribute("ProductDto");
-	    // 사용자가 클릭한 버튼에 따른 동작 구분
-	    String action = req.getParameter("action");
-	    int quantity = Integer.parseInt(req.getParameter("quantity"));
-	    
-	    
-	    if ("addToCart".equals(action)) {
-	        // 장바구니에 추가하는 처리
-	    	orderservice.insertOrder(productDto, quantity);
-            // 장바구니 추가 성공 시 처리 (예: 장바구니 페이지로 이동)
-            resp.sendRedirect(req.getContextPath() + "/market/cartSuccess.jsp");
-	        
-	    } else if ("buyNow".equals(action)) {
-	        // 바로구매 처리
-	        
-
+//	@Override
+//	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//	    ProductDto productDto = (ProductDto) req.getSession().getAttribute("ProductDto");
+//	    // 사용자가 클릭한 버튼에 따른 동작 구분
+//	    String action = req.getParameter("action");
+//	    int quantity = Integer.parseInt(req.getParameter("quantity"));
+//	    
+//	    
+//	    if ("addToCart".equals(action)) {
+//	        // 장바구니에 추가하는 처리
+//	    	orderservice.insertOrder(productDto, quantity);
+//            // 장바구니 추가 성공 시 처리 (예: 장바구니 페이지로 이동)
+//            resp.sendRedirect(req.getContextPath() + "/market/cartSuccess.jsp");
+//	        
+//	    } else if ("buyNow".equals(action)) {
+//	        // 바로구매 처리
+//	        
+//
 //	        if (success) {
 //	            // 주문 성공 시 처리 (예: 주문 완료 페이지로 이동)
 //	            resp.sendRedirect(req.getContextPath() + "/market/orderSuccess.jsp");
@@ -57,11 +57,11 @@ public class ViewController extends HttpServlet{
 //	            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/market/view.jsp");
 //	            dispatcher.forward(req, resp);
 //	        }
-	    } else {
-	        // 알 수 없는 action에 대한 처리 (예: 에러 페이지로 이동)
-	        resp.sendRedirect(req.getContextPath() + "/market/error.jsp");
-	    }
-	}
+//	    } else {
+//	        // 알 수 없는 action에 대한 처리 (예: 에러 페이지로 이동)
+//	        resp.sendRedirect(req.getContextPath() + "/market/error.jsp");
+//	    }
+//	}
 
 	
 }
