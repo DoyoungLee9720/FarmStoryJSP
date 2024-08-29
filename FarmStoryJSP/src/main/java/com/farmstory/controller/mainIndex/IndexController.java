ba16a93e454f7d6f4099f142d4c3547c48c3964c
chldudwls <dudwls4251@gmail.com>
2024-08-29 16:33:24 +0900

    CYJ_2024_08_29_index_update

--- FarmStoryJSP/src/main/java/com/farmstory/controller/mainIndex/IndexController.java
+++ FarmStoryJSP/src/main/java/com/farmstory/controller/mainIndex/IndexController.java
@@ -25,7 +25,6 @@ public class IndexController extends HttpServlet{
 	
 	private ProductService service = ProductService.INSTANCE;
 	private ArticleService articleService = ArticleService.INSTANCE;
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	    String pg = req.getParameter("pg");
