177c5ea97897fdb06c14a48d081e5c4cd425626f
Ju Gyeong <jptrnmrs@gmail.com>
2024-08-29 09:06:26 +0900

    Update OrderDao.java

--- FarmStoryJSP/src/main/java/com/farmstory/dao/OrderDao.java
+++ FarmStoryJSP/src/main/java/com/farmstory/dao/OrderDao.java
@@ -25,6 +25,32 @@ public class OrderDao extends DBHelper{
 	public List<OrderDto> selectOrders(){
 		return null;
 	}
+	public List<OrderDto> selectOrdersForAdminPage(int start){
+		List<OrderDto> orders = new ArrayList<>();
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_ORDERS_PAGED);
+			psmt.setInt(1, start);
+			rs = psmt.executeQuery();
+			while(rs.next()){
+				OrderDto order = new OrderDto();
+				order.setOrderno(rs.getInt(1));
+				order.setOrderproname(rs.getString(2));
+				order.setOrderproprice(rs.getInt(3));
+				order.setOrderstock(rs.getInt(4));
+				order.setOrderprodeliveryfee(rs.getInt(5));
+				order.setOrderpayment(rs.getInt(6));
+				order.setOrderusername(rs.getString(7));
+				order.setOrderdatetime(rs.getString(8));
+				orders.add(order);
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}finally {
+			closeAll();
+		}
+		return orders;
+	}
 	public void updateOrder(OrderDto order) {
 		
 	}
